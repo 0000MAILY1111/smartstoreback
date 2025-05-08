b15d390b17185febba4447bcffa03c3c59a77f46
0000MAILY1111 <valdezmelizza19@gmail.com>
2025-05-08 12:45:55 -0400

    categories up

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [CategoriesModule],
