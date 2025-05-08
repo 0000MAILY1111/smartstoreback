698c436512f010c8e513df549c772ba69edf1e00
0000MAILY1111 <valdezmelizza19@gmail.com>
2025-05-08 17:25:26 -0400

    configtype

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,7 +1,15 @@
 import { IsString, IsNotEmpty } from 'class-validator';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
-export const typeOrmConfig = () : TypeOrmModuleOptions => ({
+export const typeOrmConfig = (configService : ConfigService) : TypeOrmModuleOptions => ({
     type: 'postgres',
+    host: configService.get('DATABASE_HOST'),
+    port: configService.get('DATABASE_PORT'),
+    username: configService.get('DATABASE_USER'),
+    password: configService.get('DATABASE_PASSWORD'),
+    database: configService.get('DATABASE_NAME'),
+    ssl : true ,
+    logging: true,
 
 })
